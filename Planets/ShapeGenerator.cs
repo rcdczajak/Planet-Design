1cb3325d3cbd7b201ce1a4bb68e792395f1d737b
Randall Czajak <108299837+rcdczajak@users.noreply.github.com>
2024-06-08 21:33:47 +0000

    Commented ShapeGenerator.cs

--- Planets/ShapeGenerator.cs
+++ Planets/ShapeGenerator.cs
@@ -1,18 +1,26 @@
+// Necessary Unity Installments
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Class responsible for generating size and shape of planet
 public class ShapeGenerator
 {
+    // Initializes settings variables
     ShapeSettings settings;
 
+    // Constructor Class
     public ShapeGenerator (ShapeSettings settings)
     {
+        // Assigning settings
         this.settings = settings;
     }
 
+    // Method to calculate a point on the planet's surface given a point on a unit sphere
     public Vector3 CalculatePointOnPlanet (Vector3 pointOnUnitSphere)
     {
+
+        // Returns the distance of the specified point according to the planets radius
         return pointOnUnitSphere * settings.planetRadius;
     }
 }
